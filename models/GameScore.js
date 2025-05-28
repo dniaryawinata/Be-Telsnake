846f07fafa2c69b5b939931deb3d208922ecc8b2
Doni Arya <154965368+dniaryawinata@users.noreply.github.com>
2025-05-28 19:37:31 +0700

    Update GameScore.js

--- models/GameScore.js
+++ models/GameScore.js
@@ -37,7 +37,7 @@ const GameScore = {
                         query += ' AND DATE(gs.date) = CURDATE()';
                         break;
                     case 'thisWeek':
-                        query += ' AND WEEK(gs.date) = WEEK(CURDATE())';
+                        query += ' AND WEEK(gs.dte) = WEEK(CURDATE())';
                         break;
                     case 'thisMonth':
                         query += ' AND MONTH(gs.date) = MONTH(CURDATE())';
